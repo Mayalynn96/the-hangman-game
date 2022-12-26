130850a20fc617b5a79e51b45b9cba2d068d3b28
Maya Lynn <mayalynncohen@gmail.com>
2022-12-25 22:53:46 -0800

    added letter tracker and styling

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 //1. Create a Pool of words ✔
 //  - Create a var with an array ✔
-var wordPool = ["salmon", "bear", "oven", "televison", "chewing", "drinking", "hiking", "christmas", "tabletop", "eventlistener", "hangman", "playing", "computer"];
+var wordPool = ["salmon", "bear", "oven", "televison", "chewing", "drinking", "hiking", "christmas", "tabletop", "eventlistener", "hangman", "playing", "computer", "anaconda", "zebra", "snowboarding", "skiing", "mountain", "ocean", "forest", "concert", "handmade", "wife", "husband", "lion", "broccoli", "potatoes", "lemon", "steak", "hermit", "bathroom", "livingroom", "bathroom"];
 //2. Create a countdown ✔
 var timerEl = document.querySelector("#timer");
 var playEl = document.querySelector("#play");
 var wordEl = document.querySelector("#word");
 var startBtn = document.querySelector("#start");
 var letterEl = document.querySelector("#letter");
+var alphabetEl = document.querySelector("#alphabet")
 var timerInterval;
 var timeTotal = 60;
 var timeLeft = timeTotal;
 var randomWord;
 var maskedWord = [];
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //6. When a game is done ✔
 //  - scores will be kept ✔
@@ -27,7 +29,9 @@ var lossesEl = document.querySelector("#losses");
 startBtn.addEventListener("click", function() {
     // When game is ready to start when play buttonn is pressed
     if(timeLeft === timeTotal) {
-        randomizer();   
+        randomizer();  
+        letterEl.textContent = "Go!";
+        alphabetEl.textContent = alphabet.join(" ");
         wordEl.textContent = maskedWord.join(" ");
         startBtn.textContent = "restart";
         playEl.textContent = "Press restart to play again";
@@ -35,9 +39,11 @@ startBtn.addEventListener("click", function() {
         // to avoid counter going crazy if pressed to quickly in a row
         timeLeft = timeTotal - 1;  
         return countdown(); 
-        // When replay button is pressed
+        // When restart button is pressed
     } else {
         clearInterval(timerInterval);
+        alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        alphabetEl.textContent = alphabet.join(" ");
         timeLeft = timeTotal;
         timerEl.textContent = "You have " + timeLeft + " seconds left.";
         startBtn.textContent = "start";  
@@ -60,6 +66,7 @@ function countdown() {
             //  - winning or loosing message shows up + score kept ✔
             if(timeLeft <= 0) {
                 clearInterval(timerInterval);
+                timerEl.textContent = "You have 0 seconds left.";
                 wordEl.textContent = randomWord;
                 letterEl.textContent = "You ran out of time!"
                 losses++;
@@ -110,6 +117,16 @@ function letterInWord() {
     }
 }
 
+function letterUsed() {
+    for(var i = 0; i < alphabet.length; i++) {
+        if(alphabet[i] === key.toUpperCase()) {
+            alphabet.splice([i], 1, "_");
+            alphabetEl.textContent = alphabet.join(" ");
+            return;
+        }
+    }
+}
+
 // registers when a key is pressed and returns the function letterInWord if counter is started
 document.addEventListener("keydown", keydownAction);
 
@@ -117,7 +134,8 @@ function keydownAction(event) {
     key = event.key.toLowerCase();
     letterEl.textContent = key;
     if(timeLeft < 120 || timeLeft > 0) {
-       return letterInWord();
+        letterUsed();
+        return letterInWord();
     }
 }
 
